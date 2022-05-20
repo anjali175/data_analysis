b26ebd1ade53d8edc7be262ed481f03cd91b8198
anjali175 <71869275+anjali175@users.noreply.github.com>
2022-05-21 00:01:14 +0530

    second commit

--- webapp_test.py
+++ webapp_test.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from streamlit_folium import folium_static
 import folium
 import plotly.express as px
